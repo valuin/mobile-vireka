3eead3caf0de9f1dd15f888ce535798f4b55937c
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2025-05-30 22:34:48 +0700

    initial commit & repo starter

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,22 +3,18 @@ import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-nativ
 
 type ButtonProps = {
   title: string;
+  className?: string; // Add className to props
 } & TouchableOpacityProps;
 
-export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps }, ref) => {
+export const Button = forwardRef<View, ButtonProps>(({ title, className, ...touchableProps }, ref) => {
   return (
     <TouchableOpacity
       ref={ref}
       {...touchableProps}
-      className={`${styles.button} ${touchableProps.className}`}>
-      <Text className={styles.buttonText}>{title}</Text>
+      className={`items-center bg-indigo-500 rounded-[28px] shadow-md p-4 ${className || ''}`}>
+      <Text className="text-white text-lg font-semibold text-center">{title}</Text>
     </TouchableOpacity>
   );
 });
 
 Button.displayName = 'Button';
-
-const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
-  buttonText: 'text-white text-lg font-semibold text-center',
-};
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,6 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or Anon Key environment variables.');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
